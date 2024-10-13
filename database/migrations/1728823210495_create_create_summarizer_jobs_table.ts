36c4bc5c53d9e6c5d852bfe9850612447d450fab
Ralph Kevin Y. Casipe <ralphkevincasipe@yahoo.com>
2024-10-13 20:49:57 +0800

    :fire: remove unnecessary codebase

---
+++ database/migrations/1728823210495_create_create_summarizer_jobs_table.ts
@@ -0,0 +1,23 @@
+import { SummarizerJobStatus } from '#models/summarizer_job'
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'summarizer_jobs'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('url').index().notNullable()
+      table
+        .enum('status', Object.values(SummarizerJobStatus))
+        .defaultTo(SummarizerJobStatus.PENDING)
+      table.text('summary')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
