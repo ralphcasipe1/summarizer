bb46275eb154bda7b6dd78641125513ead369263
Ralph Kevin Y. Casipe <ralphkevincasipe@yahoo.com>
2024-10-14 22:32:06 +0800

    :rotating_light: fix lint by reverting some of the code related to user and add dangling commas

---
+++ app/models/user.ts
@@ -0,0 +1,26 @@
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+/**
+ * In this proof-of-concept, we don't need this model.
+ */
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  static accessTokens = DbAccessTokensProvider.forModel(User)
+}
